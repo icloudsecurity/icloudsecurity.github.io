67394dadfb36af3093e5f03a86e7072173e68a67
~$ sudo <84492503+postedby@users.noreply.github.com>
2022-03-20 19:51:18 +0100

    Add files via upload

---
+++ assets/js/modal.js
@@ -0,0 +1,20 @@
+
+var modal = document.getElementById('viewSnap');
+
+var btn = document.getElementById("openModal");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
